5efc6f42d44c3aac5217c3af4926b64f41ae9510
JBoulenger21 <boulenger.julie@gmail.com>
2020-09-11 11:17:50 +0200

    Création en cours de singleton pour la connexion à la base de données

--- model.php
+++ model.php
@@ -1,4 +1,19 @@
 <?php
+class connexionBDD {
+  private $_instance = null;
+  private $ConnexionBaseDeDonnees = '';
+  private function connexion($ConnexionBaseDeDonnees){
+    $this->$ConnexionBaseDeDonnees;
+  }
+  public static function getConnexion($ConnexionBaseDeDonnees){
+    if(isnull(self::$_instance)){
+      self::$_instance = new connexionBDD($ConnexionBaseDeDonnees);
+    }
+    return self::$_instance;
+  }
+}
+
+
 $servername = "localhost";
 $dbname = "spacedrivebdd";
 $username = "root";
@@ -25,16 +40,14 @@ function newTableService(){
   } catch(PDOException $e) {
     die('erreur :'.$e->getMessage());
   }
-
-$query = "CREATE TABLE IF NOT EXISTS `Service` (
+  $query = "CREATE TABLE IF NOT EXISTS `Service` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT , `titre` VARCHAR(255) NOT NULL ,
   `icone` VARCHAR(255) NOT NULL , `description` VARCHAR(255) , `plan` INT, PRIMARY KEY (`id`)) ENGINE = MyISAM;
-)";
+  )";
 
-$request = $dB->prepare($query);
-$request->execute();
-$request->closeCursor();
-return true;
+  $request = $dB->prepare($query);
+  $request->execute();
+  $request->closeCursor();
 }
 function newTableNosServices(){
   $servername = "localhost";
@@ -57,7 +70,23 @@ function newTableNosServices(){
   $request = $dB->prepare($query);
   $request->execute();
   $request->closeCursor();
-  return true;
 
+}
+function newTable(){
+  $servername = "localhost";
+  $dbname = "spacedrivebdd";
+  $username = "root";
+  $password = "";
+
+  try{
+    $dB = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    $dB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch(PDOException $e) {
+    die('erreur :'.$e->getMessage());
+  }
+  newTableService();
+  newTableNosServices();
+  return true;
 }
 ?>
--- traitement.php
+++ traitement.php
@@ -3,7 +3,7 @@ require('model.php');
 
 if(isset($_POST['newTable'])){
   var_dump(newTableService());
-  if(newTableService() && newTableNosServices()){
+  if(newTable()){
     echo "c'est ok";
   }else{
     echo "c'est pas ok";
